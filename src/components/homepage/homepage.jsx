07a65496e0fa53066631912132bfd8f5e8b35e07
Jacob Zhang <walkerzjs2@gmail.com>
2020-09-03 16:28:34 +1000

    update favicon

--- src/components/homepage/homepage.jsx
+++ src/components/homepage/homepage.jsx
@@ -4,11 +4,13 @@ import Upload from "./upload/upload";
 import Header from "./header";
 import Intro from "./intro";
 import Footer from "./footer";
+
 const HomePageC = styled.div`
   position: relative;
   height: 100vh;
   min-height: 1200px;
   width: 100%;
+  /* https://pixabay.com/photos/curious-cat-bird-watching-danger-4766017/ */
   background: linear-gradient(0deg, rgba(0, 0, 0, 0.75), rgba(0, 0, 0, 0.75)),
     url("/background_homepage.jpg");
   background-size: cover;
